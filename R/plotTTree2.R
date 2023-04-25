4162f3eaddfa36c4b60222b8473f70a706dd30b1
Jessalyn Sebastian <jessalyn.sebastian@uci.edu>
2023-04-25 10:48:17 -0700

    changed type representation in plotttree2

--- R/plotTTree2.R
+++ R/plotTTree2.R
@@ -1,14 +1,15 @@
 #' Plot a transmission tree in an economic format
 #' @param ttree Transmission tree
 #' @param showLabels Boolean for whether or not to show the labels
-#' @param showMissingLinks Option for how to show missing links: (0) as dots, (1) as several gray levels, (2) as a single gray level
+#' @param hostTypeNames Vector of names for host types, ordered.
+#' @param showMissingLinks Option for how to show missing links: (0) as dots, (1) as several gray levels, (2) as a single gray level.
 #' @param maxTime Maximum value of time to show on x axis
 #' @param cex Expansion factor
 #' @return Returns invisibly the first parameter
 #' @examples 
 #' plotTTree2(extractTTree(simulateOutbreak()))
 #' @export
-plotTTree2 = function(ttree,showLabels=TRUE,showMissingLinks=0,maxTime=NA,cex=1) {
+plotTTree2 = function(ttree,showLabels=TRUE, hostTypeNames = NA,showMissingLinks=0,maxTime=NA,cex=1) {
   nam=ttree$nam
   ttree=ttree$ttree
   multitype <- ifelse( ncol(ttree) == 4, yes = T, no = F )
@@ -55,12 +56,10 @@ plotTTree2 = function(ttree,showLabels=TRUE,showMissingLinks=0,maxTime=NA,cex=1)
   ma=max(ttree[which(!is.na(ttree[,1])),1])
   if (!is.na(maxTime)) ma=maxTime
   plot(c(),c(),xlim=c(mi,ma),ylim=c(0,n+1),xlab='',ylab='')
-  if ( multitype ) pal <- grDevices::rainbow(max(ttree[,5]))
-  else pal <- grDevices::gray.colors(max(ttree[,4]))
+  pal <- grDevices::gray.colors(max(ttree[,4]))
   for (i in 1:n) {
     if (ttree[i,3]!=0) {
-      if ( multitype ) dircol <- pal[ttree[i,5]]
-      else dircol <- pal[ttree[i,4]] 
+      dircol <- pal[ttree[i,4]] 
       #arrows(ttree[i,1],ys[ttree[i,3]],ttree[i,1],ys[i],length=0)
       arrows(ttree[ttree[i,3],1],ys[ttree[i,3]],ttree[i,1],ys[i],length=0,col=dircol)
     }
@@ -69,10 +68,18 @@ plotTTree2 = function(ttree,showLabels=TRUE,showMissingLinks=0,maxTime=NA,cex=1)
     if (showLabels && !is.na(ttree[i,2])) text(ttree[i,1],ys[i],nam[i],pos=4,cex=cex)
     #lines(c(mi,ma),c(ys[i],ys[i]))
   }
+  type_colors <- grDevices::rainbow(max(ttree[,5]))
   for (i in 1:n) {
     #points(ttree[i,1],ys[i],pch=21,bg='black',cex=0.2+0.2*(!is.na(ttree[i,2])))
-    points(ttree[i,1],ys[i],pch=21,bg=ifelse(is.na(ttree[i,2]),'white','black'),cex=cex)
+    points(ttree[i,1], ys[i],
+           pch = ifelse(is.na(ttree[i,2]),21,16),
+           bg = "white",
+           col = type_colors[ttree[i,5]],
+           cex = cex)
   }
-  if (length(pal)>2) legend('topleft',legend = 0:(length(pal)-1),col = pal,lty=1,cex=cex,title='Missing links')
+  if ( length(pal)>2 ) legend('topleft',legend = 0:(length(pal)-1),col = pal,lty=1,cex=cex,title='Missing links')
+  if ( is.na(hostTypeNames) ) hostTypeNames <- paste( "Type", 1:length(type_colors ) )
+  if ( multitype ) legend('topleft', legend = hostTypeNames, fill = type_colors, title = "Host Type")
   return(invisible(ttree))
 }
+plotTTree2(tree)
